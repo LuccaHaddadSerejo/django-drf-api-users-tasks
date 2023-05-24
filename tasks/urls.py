06415af673ae14b20a8d6f42aa0a6c227a97037c
LuccaHaddadSerejo <hslucca@gmail.com>
2023-05-23 21:51:34 -0300

    fix: changing the routes to plural for REST padronizing

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("task/", views.TaskView.as_view()),
-    path("task/list/todo/", views.TaskToDoView.as_view()),
-    path("task/<int:task_id>/", views.TaskDetailView.as_view()),
+    path("tasks/", views.TaskView.as_view()),
+    path("tasks/list/todo/", views.TaskToDoView.as_view()),
+    path("tasks/<int:task_id>/", views.TaskDetailView.as_view()),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 urlpatterns = [
-    path("user/", views.UserView.as_view()),
-    path("user/<int:user_id>/", views.UserDetailView.as_view()),
-    path("user/login/", jwt_views.TokenObtainPairView.as_view()),
+    path("users/", views.UserView.as_view()),
+    path("users/<int:user_id>/", views.UserDetailView.as_view()),
+    path("users/login/", jwt_views.TokenObtainPairView.as_view()),
 ]
